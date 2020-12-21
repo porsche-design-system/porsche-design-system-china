a3cf57456b260243aed1b84f33b59f3893e68cce
rock wang <rockwang@rocks-MacBook-Pro.local>
2020-12-21 15:56:58 +0800

    docs: test; chore: dependances collection, version, rp script dependances; chore: linting rules; feat: storybook fix .etc

--- libs/react/src/kits/button/button.stories.tsx
+++ libs/react/src/kits/button/button.stories.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import Button from './index';
-import { ButtonType, ButtonTypes, SizeType, SIZES } from './types';
 import { withKnobs, text, select, color } from '@storybook/addon-knobs';
+import { ButtonType, ButtonTypes, SizeType, SIZES } from './types';
+
+import Button from './index';
 
 export default {
-  title: 'Button',
-  component: Button,
-  decorators: [withKnobs],
+	title: 'Button',
+	component: Button,
+	decorators: [withKnobs],
 };
 
 export const knobsButton = () => (
-  <Button
-    size={select<SizeType>('size', SIZES, 'large')}
-    type={select<ButtonType>('type', ButtonTypes, 'primary')}
-  >
-    {text('123', '123')}
-  </Button>
+	<Button
+		size={select<SizeType>('size', SIZES, 'large')}
+		type={select<ButtonType>('type', ButtonTypes, 'primary')}
+	>{text('123', '123')}
+	</Button>
 );
 
 export const Btn = () => <Button type="ghost">12344</Button>;
--- libs/react/typings/custom-typings.d.ts
+++ libs/react/typings/custom-typings.d.ts
@@ -1,7 +1,7 @@
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.jpeg'
-declare module '*.gif'
-declare module '*.bmp'
-declare module '*.tiff'
-declare module '@pui/react'
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.bmp';
+declare module '*.tiff';
+declare module '@pui/react';
