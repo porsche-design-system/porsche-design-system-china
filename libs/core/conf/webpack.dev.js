484401ed79847b7b02e83cf468888bbf742dbb93
rock wang <rockwang@rocks-MacBook-Pro.local>
2020-12-23 09:44:57 +0800

    chore: pui/core: lint & preddier & ts config & dev files.etc

--- libs/core/conf/webpack.dev.js
+++ libs/core/conf/webpack.dev.js
@@ -1,6 +1,6 @@
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -20,4 +20,4 @@ module.exports = merge(common, {
       'process.env.NODE_ENV': JSON.stringify('development'),
     }),
   ],
-})
+});
---
+++ libs/core/typings/custom-typings.d.ts
@@ -0,0 +1,1 @@
+declare module '@pui/core'
