ab37372dc7fcd2f0ddeb0adbcb4c2e4b36d65c76
Calidan <calidan@vip.qq.com>
2021-03-27 17:52:20 +0800

    更新button功能

---
+++ libs/react/src/components/radio/radio-group.tsx
@@ -0,0 +1,68 @@
+import React, { ChangeEventHandler, CSSProperties, useEffect, useRef } from 'react';
+import { componentClassNames } from '../../shared/class-util';
+import './radio-group.scss';
+
+export interface Props {
+  // 组件属性 //
+
+  /** 类名 */
+  className?: string;
+  /** 样式 */
+  style?: CSSProperties;
+
+  /* 值 */
+  value?: string | number;
+
+  /** 是否禁用 */
+  disabled?: boolean;
+
+  /* 点击事件 */
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+
+  /** 子组件 */
+  children?: React.ReactNode;
+}
+
+let idCounter = 0;
+const generateId = () => {
+  idCounter++;
+  return 'radio-group-' + idCounter;
+};
+const RadioGroup = ({ className, style, disabled = false, children, onChange, value }: Props) => {
+  const radioButtons = useRef<HTMLInputElement[]>();
+
+  const refLoaded = (radioGroup: HTMLDivElement) => {
+    radioButtons.current = [].slice.call(radioGroup.getElementsByTagName('input'));
+    const radioGroupName = generateId();
+    radioButtons.current.forEach(radioButton => {
+      radioButton.onchange = onChange as any;
+      radioButton.name = radioGroupName;
+      if (value) {
+        radioButton.checked = radioButton.value === value;
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (radioButtons.current) {
+      radioButtons.current.forEach(radioButton => {
+        radioButton.onchange = onChange as any;
+        if (value) {
+          radioButton.checked = radioButton.value === value;
+        }
+      });
+    }
+  }, [value]);
+
+  return (
+    <div
+      className={componentClassNames('pui-radio-group', { disabled: disabled + '' }, className)}
+      style={style}
+      ref={refLoaded}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { RadioGroup };
--- libs/react/src/storybook/component-doc/radio.stories.tsx
+++ libs/react/src/storybook/component-doc/radio.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Radio } from '../..';
+import { Radio, RadioGroup } from '../..';
 
 export default {
   title: 'General/Radio',
@@ -9,10 +9,31 @@ export default {
 export const RadioStoryBook = () => {
   return (
     <div>
-      <Radio label="选项1" />
-      <Radio label="选项2" />
-      <Radio label="选项3" />
-      <Radio label="选项4" disabled />
+      <RadioGroup
+        onChange={evt => {
+          console.log(evt.target.value);
+        }}
+        value="val3"
+      >
+        <Radio label="选项1" value="val1" checked />
+        <Radio label="选项2" value="val2" />
+        <Radio label="选项3" value="val3" />
+        <Radio label="选项4" value="val4" disabled />
+      </RadioGroup>
+
+      <RadioGroup>
+        <Radio label="选项1" />
+        <Radio label="选项2" />
+        <Radio label="选项3" />
+        <Radio label="选项4" disabled />
+      </RadioGroup>
+
+      <RadioGroup disabled>
+        <Radio label="选项1" />
+        <Radio label="选项2" />
+        <Radio label="选项3" />
+        <Radio label="选项4" disabled />
+      </RadioGroup>
     </div>
   );
 };
