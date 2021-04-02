6a39b9cbc7d44e3f2c5aaa3e51118fdd433fc807
liu yi <lilugirl2005@126.com>
2021-04-03 02:22:59 +0800

    feature: 模式框

--- libs/react/src/storybook/component-doc/modal.stories.tsx
+++ libs/react/src/storybook/component-doc/modal.stories.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import { Modal } from '../../';
+import React, { useState } from 'react';
+import { Button, Input, Modal } from '../../';
 export default {
   title: 'Feedback/Modal',
   component: Modal
 };
 
 export const ModalStoryBook = () => {
-  return <Modal></Modal>;
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const handleOk=()=>{
+    setIsModalVisible(false);
+  }
+
+  const handleCancel=()=>{
+    setIsModalVisible(false);
+  }
+  return <>
+  <Modal title='模式框标题' visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+    内容
+    <p>
+    <Input label="用户名" />
+    </p>
+    <p>
+      <Input label="密码" />
+    </p>
+  </Modal>
+  <Button onClick={()=>setIsModalVisible(true)} type="primary">显示模式框</Button>
+  </>;
 };
 
 ModalStoryBook.storyName = 'Modal';
