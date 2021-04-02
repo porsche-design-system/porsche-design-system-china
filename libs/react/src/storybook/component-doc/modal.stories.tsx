e4c9046027f30219ce9cb7b9e81f9129ead11b88
liu yi <lilugirl2005@126.com>
2021-04-03 02:36:22 +0800

    chore: 清理样式

--- libs/react/src/storybook/component-doc/modal.stories.tsx
+++ libs/react/src/storybook/component-doc/modal.stories.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Input, Modal } from '../../';
+import { Button, Modal } from '../../';
 export default {
   title: 'Feedback/Modal',
   component: Modal
@@ -8,21 +8,30 @@ export default {
 export const ModalStoryBook = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
 
-  const handleOk=()=>{
+  const handleOk = () => {
     setIsModalVisible(false);
-  }
+  };
 
-  const handleCancel=()=>{
+  const handleCancel = () => {
     setIsModalVisible(false);
-  }
-  return <>
-  <Modal title='用户协议' okText='同意' cancelText='我考虑一下' visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-   
-      根据中华人民共和国...
-   
-  </Modal>
-  <Button onClick={()=>setIsModalVisible(true)} type="primary">显示模式框</Button>
-  </>;
+  };
+  return (
+    <>
+      <Modal
+        title="用户协议"
+        okText="同意"
+        cancelText="我考虑一下"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        根据中华人民共和国...
+      </Modal>
+      <Button onClick={() => setIsModalVisible(true)} type="primary">
+        显示模式框
+      </Button>
+    </>
+  );
 };
 
 ModalStoryBook.storyName = 'Modal';
