55ffb55c96b31150f8ecf4fde05310d6fb2b9b7d
Calidan <calidan@vip.qq.com>
2021-04-15 23:15:08 +0800

    fix: 更新Table

--- libs/react/src/components/table/table.tsx
+++ libs/react/src/components/table/table.tsx
@@ -1,7 +1,17 @@
-import React, { CSSProperties, useRef } from 'react';
+import React, { CSSProperties, useRef, useState } from 'react';
 import { componentClassNames } from '../../shared/class-util';
 import { CheckBox } from '../checkbox/checkbox';
 import './table.scss';
+
+export interface TableColumn {
+  title?: string;
+  key?: string;
+  width?: string;
+  fixed?: 'none' | 'left' | 'right';
+  multiline?: boolean;
+  sortable?: boolean;
+  customCell?: (rowData: any) => React.ReactNode;
+}
 
 export interface TableProps {
   /* 类名 */
@@ -10,17 +20,10 @@ export interface TableProps {
   style?: CSSProperties;
 
   /* 列属性 */
-  columns?: {
-    key?: string;
-    name?: string;
-    width?: string;
-    fixed?: 'none' | 'left' | 'right';
-    sortable?: boolean;
-    customCell: (rowData: any) => React.ReactNode;
-  }[];
+  columns: TableColumn[];
 
   /* 数据 */
-  data?: any[];
+  data: any[];
 
   /* 可选定行 */
   pickable?: boolean;
@@ -33,7 +36,11 @@ export interface TableProps {
  * Primary UI component for user interaction
  */
 const Table = ({ className, style, columns, data, onSort, pickable = false }: TableProps) => {
-  const ddd = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, , 1, 1, 1, 1];
+  const middleColumns = columns;
+  const leftColumns = [];
+  const rightColumns = [];
+
+  const [isScrollLeft, setIsScrollLeft] = useState(false);
 
   const headRef = useRef<any>(null);
   const bodyRef = useRef<any>(null);
@@ -47,55 +54,85 @@ const Table = ({ className, style, columns, data, onSort, pickable = false }: Ta
         const variation = parseInt(evt.deltaX);
         headRef.current.scrollLeft = headRef.current.scrollLeft + variation;
         bodyRef.current.scrollLeft = headRef.current.scrollLeft;
+        setIsScrollLeft(bodyRef.current.scrollLeft !== 0);
       };
     }
   };
 
+  const tableBodyRefLoaded = (elem: HTMLDivElement) => {
+    if (elem) {
+      bodyRef.current = elem;
+      elem.onscroll = (evt: any) => {
+        if (headRef.current) {
+          headRef.current.scrollLeft = evt.target.scrollLeft;
+          setIsScrollLeft(bodyRef.current.scrollLeft !== 0);
+        }
+      };
+    }
+  };
+
+  const tableWidth = (middleColumns.length + 1) * 200 + 'px';
+  const tableHeight = 60 * 10 + 'px';
+
   return (
-    <div className={componentClassNames('pui-table', {}, className)} style={style}>
+    <div
+      className={componentClassNames('pui-table', { scrollLeft: isScrollLeft + '' }, className)}
+      style={style}
+    >
       <div ref={wheelMove}>
         <div ref={headRef} className="pui-table-head">
-          <table>
-            <tr>
-              <td className="measure"></td>
-              <td className="measure" style={{ width: '1000px' }}></td>
-              <td className="measure" style={{ width: '1000px' }}></td>
-              <td className="measure"></td>
-            </tr>
-            <tr>
-              <td className="fixed">选定</td>
-              <td>经销商</td>
-              <td>营业额</td>
-              <td>操作</td>
-            </tr>
+          <table style={{ width: tableWidth }}>
+            <tbody>
+              <tr>
+                <td className="measure" style={{ width: '80px' }} />
+                {middleColumns.map((column, inx) => (
+                  <td
+                    key={'measure' + inx}
+                    className="measure"
+                    style={{ width: column.width || '200px' }}
+                  />
+                ))}
+              </tr>
+              <tr>
+                <td className="fixed">选定</td>
+                {middleColumns.map((column, inx) => (
+                  <td key={'head' + inx}>{column.title}</td>
+                ))}
+              </tr>
+            </tbody>
           </table>
         </div>
-        <div
-          className="pui-table-body"
-          ref={bodyRef}
-          onScroll={(evt: any) => {
-            if (headRef.current) {
-              headRef.current.scrollLeft = evt.target.scrollLeft;
-            }
-          }}
-        >
-          <table>
-            <tr>
-              <td className="measure"></td>
-              <td className="measure" style={{ width: '1000px' }}></td>
-              <td className="measure" style={{ width: '1000px' }}></td>
-              <td className="measure"></td>
-            </tr>
-            {ddd.map(() => (
+        <div className="pui-table-body" ref={tableBodyRefLoaded} style={{ height: tableHeight }}>
+          <table style={{ width: tableWidth }}>
+            <tbody>
               <tr>
-                <td className="fixed">
-                  <CheckBox size="small" />
-                </td>
-                <td>上海浦东经销商</td>
-                <td>12000</td>
-                <td>操作</td>
+                <td className="measure" style={{ width: '80px' }} />
+                {middleColumns.map((column, inx) => (
+                  <td
+                    key={'measure' + inx}
+                    className="measure"
+                    style={{ width: column.width || '200px' }}
+                  />
+                ))}
               </tr>
-            ))}
+              {data.map((rowData, inx) => (
+                <tr key={'row' + inx}>
+                  <td className="fixed">
+                    <CheckBox size="small" />
+                  </td>
+                  {middleColumns.map((column, inx) => (
+                    <td
+                      key={'col' + inx}
+                      className={column.multiline ? 'pui-table-cell-multiline' : ''}
+                    >
+                      {column.customCell
+                        ? column.customCell(rowData)
+                        : column.key && rowData[column.key]}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
--- libs/react/src/storybook/component-doc/table.stories.tsx
+++ libs/react/src/storybook/component-doc/table.stories.tsx
@@ -1,15 +1,46 @@
 import React from 'react';
-import { Table } from '../..';
+import { Table, Button, TableColumn } from '../..';
 
 export default {
   title: 'Data Display/Table',
   component: Table
 };
 
+const rowData = {
+  dealerName: '上海浦东保时捷中心',
+  dealerCode: '1000000',
+  phoneNumber: '021-22211111',
+  address: '上海市东方路123号',
+  workingTime: '10:00AM - 10:30PM',
+  saleModel: '911 718 Taycan Panamera Macan Cayenne'
+};
+
+const tableData: any[] = [];
+for (let i = 0; i < 15; i++) {
+  tableData.push(rowData);
+}
+
+const columns: TableColumn[] = [
+  { title: '经销商', key: 'dealerName' },
+  { title: '号码', customCell: (rowData: any) => <u>{rowData.dealerCode}</u> },
+  { title: '联系号码', key: 'phoneNumber' },
+  { title: '地址', key: 'address' },
+  { title: '工作时间', key: 'workingTime' },
+  { title: '在售车型', key: 'saleModel' },
+  {
+    title: '操作',
+    customCell: () => (
+      <>
+        <Button type="text">修改</Button>
+      </>
+    )
+  }
+];
+
 export const TableStoryBook = () => {
   return (
     <div>
-      <Table />
+      <Table data={tableData} columns={columns} />
     </div>
   );
 };
