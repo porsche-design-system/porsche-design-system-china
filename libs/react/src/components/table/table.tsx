b789816f9c40e7ac0130f6fa195c57c57900453a
Calidan <calidan@vip.qq.com>
2021-04-15 18:08:37 +0800

    fix: 更新Table

--- libs/react/src/components/table/table.tsx
+++ libs/react/src/components/table/table.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import React, { CSSProperties, useRef } from 'react';
 import { componentClassNames } from '../../shared/class-util';
 import { CheckBox } from '../checkbox/checkbox';
 import './table.scss';
@@ -35,48 +35,70 @@ export interface TableProps {
 const Table = ({ className, style, columns, data, onSort, pickable = false }: TableProps) => {
   const ddd = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, , 1, 1, 1, 1];
 
+  const headRef = useRef<any>(null);
+  const bodyRef = useRef<any>(null);
+
+  const wheelMove = (tableContainer: HTMLDivElement) => {
+    if (tableContainer) {
+      tableContainer.onwheel = (evt: any) => {
+        if (headRef.current.scrollLeft === 0 && evt.deltaX < 0) {
+          evt.preventDefault();
+        }
+        const variation = parseInt(evt.deltaX);
+        headRef.current.scrollLeft = headRef.current.scrollLeft + variation;
+        bodyRef.current.scrollLeft = headRef.current.scrollLeft;
+      };
+    }
+  };
+
   return (
     <div className={componentClassNames('pui-table', {}, className)} style={style}>
-      <table className="pui-table-fixed-left">
-        <tr>
-          <th>选定</th>
-        </tr>
-        {ddd.map(() => (
-          <tr>
-            <td>
-              <CheckBox size="small" />
-            </td>
-          </tr>
-        ))}
-      </table>
-      <div className="pui-table-middle">
-        <table className="pui-table-main">
-          <thead>
+      <div ref={wheelMove}>
+        <div ref={headRef} className="pui-table-head">
+          <table>
             <tr>
-              <th>经销商</th>
-              <th>营业额</th>
+              <td className="measure"></td>
+              <td className="measure" style={{ width: '1000px' }}></td>
+              <td className="measure" style={{ width: '1000px' }}></td>
+              <td className="measure"></td>
             </tr>
-          </thead>
-          <tbody style={{ height: '200px', overflow: 'auto', display: 'block' }}>
+            <tr>
+              <td className="fixed">选定</td>
+              <td>经销商</td>
+              <td>营业额</td>
+              <td>操作</td>
+            </tr>
+          </table>
+        </div>
+        <div
+          className="pui-table-body"
+          ref={bodyRef}
+          onScroll={(evt: any) => {
+            if (headRef.current) {
+              headRef.current.scrollLeft = evt.target.scrollLeft;
+            }
+          }}
+        >
+          <table>
+            <tr>
+              <td className="measure"></td>
+              <td className="measure" style={{ width: '1000px' }}></td>
+              <td className="measure" style={{ width: '1000px' }}></td>
+              <td className="measure"></td>
+            </tr>
             {ddd.map(() => (
               <tr>
+                <td className="fixed">
+                  <CheckBox size="small" />
+                </td>
                 <td>上海浦东经销商</td>
                 <td>12000</td>
+                <td>操作</td>
               </tr>
             ))}
-          </tbody>
-        </table>
+          </table>
+        </div>
       </div>
-      <table className="pui-table-fixed-right">
-        <tr>
-          <th>操作</th>
-        </tr>
-        {ddd.map(() => (
-          <tr>
-            <td>删除</td>
-          </tr>
-        ))}
-      </table>
     </div>
   );
 };
