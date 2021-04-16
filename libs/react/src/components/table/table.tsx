93932c0a74e58628ce423c959828854a81ace7b6
Calidan <calidan@vip.qq.com>
2021-04-16 11:46:14 +0800

    feat: table样式优化

--- libs/react/src/components/table/table.tsx
+++ libs/react/src/components/table/table.tsx
@@ -6,7 +6,7 @@ import './table.scss';
 export interface TableColumn {
   title?: string;
   key?: string;
-  width?: string;
+  width?: number;
   fixed?: 'none' | 'left' | 'right';
   multiline?: boolean;
   sortable?: boolean;
@@ -26,7 +26,7 @@ export interface TableProps {
   data: any[];
 
   /* 可选定行 */
-  pickable?: boolean;
+  selectable?: boolean;
 
   /* 排序事件 */
   onSort?: (columnName: string) => void;
@@ -35,12 +35,23 @@ export interface TableProps {
 /**
  * Primary UI component for user interaction
  */
-const Table = ({ className, style, columns, data, onSort, pickable = false }: TableProps) => {
-  const middleColumns = columns;
-  const leftColumns = [];
-  const rightColumns = [];
+const Table = ({ className, style, columns, data, onSort, selectable = false }: TableProps) => {
+  const middleColumns: TableColumn[] = [];
+  const leftColumns: TableColumn[] = [];
+  const rightColumns: TableColumn[] = [];
+
+  columns.forEach(col => {
+    if (col.fixed === 'left') {
+      leftColumns.push(col);
+    } else if (col.fixed === 'right') {
+      rightColumns.push(col);
+    } else if (col.fixed === 'none' || !col.fixed) {
+      middleColumns.push(col);
+    }
+  });
 
   const [isScrollLeft, setIsScrollLeft] = useState(false);
+  const [isScrollRight, setIsScrollRight] = useState(false);
 
   const headRef = useRef<any>(null);
   const bodyRef = useRef<any>(null);
@@ -48,13 +59,27 @@ const Table = ({ className, style, columns, data, onSort, pickable = false }: Ta
   const wheelMove = (tableContainer: HTMLDivElement) => {
     if (tableContainer) {
       tableContainer.onwheel = (evt: any) => {
-        if (headRef.current.scrollLeft === 0 && evt.deltaX < 0) {
+        const variationX = parseInt(evt.deltaX);
+        const variationY = parseInt(evt.deltaY);
+
+        headRef.current.scrollLeft += variationX;
+        bodyRef.current.scrollLeft = headRef.current.scrollLeft;
+        bodyRef.current.scrollTop += variationY;
+
+        const scrollXPercentage =
+          headRef.current.scrollLeft /
+          (bodyRef.current.children[0].offsetWidth - bodyRef.current.offsetWidth);
+
+        setIsScrollLeft(scrollXPercentage > 0.01);
+        setIsScrollRight(scrollXPercentage < 0.99);
+
+        const scrollYPercentage =
+          bodyRef.current.scrollTop /
+          (bodyRef.current.children[0].offsetHeight - bodyRef.current.offsetHeight);
+
+        if (scrollYPercentage > 0.01 && scrollYPercentage < 0.99) {
           evt.preventDefault();
         }
-        const variation = parseInt(evt.deltaX);
-        headRef.current.scrollLeft = headRef.current.scrollLeft + variation;
-        bodyRef.current.scrollLeft = headRef.current.scrollLeft;
-        setIsScrollLeft(bodyRef.current.scrollLeft !== 0);
       };
     }
   };
@@ -71,12 +96,83 @@ const Table = ({ className, style, columns, data, onSort, pickable = false }: Ta
     }
   };
 
-  const tableWidth = (middleColumns.length + 1) * 200 + 'px';
+  const defaultWidth = 200;
+  const selectColumnWidth = 50;
+  const tableWidth =
+    middleColumns.length * defaultWidth + (selectable ? selectColumnWidth : 0) + 'px';
   const tableHeight = 60 * 10 + 'px';
+
+  const renderMeasureCell = (column: TableColumn, inx: number) => {
+    return (
+      <td
+        key={'measure' + inx}
+        className={'pui-table-measure'}
+        style={{ width: column.width || defaultWidth + 'px' }}
+      />
+    );
+  };
+
+  const renderTitleCell = (
+    column: TableColumn,
+    inx: number,
+    fixed: 'left' | 'right' | null,
+    style: CSSProperties
+  ) => {
+    return (
+      <td key={'head' + inx} className={fixed ? 'pui-table-fixed-' + fixed : ''} style={style}>
+        {column.title || ''}
+      </td>
+    );
+  };
+
+  const renderDataCell = (
+    column: TableColumn,
+    inx: number,
+    fixed: 'left' | 'right' | null,
+    rowData: any,
+    style: CSSProperties
+  ) => {
+    return (
+      <td
+        key={'col' + inx}
+        className={
+          (column.multiline ? 'pui-table-cell-multiline' : '') +
+          ' ' +
+          (fixed ? 'pui-table-fixed-' + fixed : '')
+        }
+        style={style}
+      >
+        {column.customCell ? column.customCell(rowData) : column.key && rowData[column.key]}
+      </td>
+    );
+  };
+
+  const fixColumnLeft: number[] = [0];
+  let accumulateLeft = 0;
+  if (selectable) {
+    accumulateLeft += selectColumnWidth;
+    fixColumnLeft.push(accumulateLeft);
+  }
+  leftColumns.forEach(col => {
+    accumulateLeft += col.width || defaultWidth;
+    fixColumnLeft.push(accumulateLeft);
+  });
+
+  const fixColumnRight: number[] = [0];
+  let accumulateRight = 0;
+  rightColumns.forEach(col => {
+    accumulateRight += col.width || defaultWidth;
+    fixColumnRight.push(accumulateRight);
+  });
+  fixColumnRight.reverse().splice(0, 1);
 
   return (
     <div
-      className={componentClassNames('pui-table', { scrollLeft: isScrollLeft + '' }, className)}
+      className={componentClassNames(
+        'pui-table',
+        { scrollLeft: isScrollLeft + '', scrollRight: isScrollRight + '' },
+        className
+      )}
       style={style}
     >
       <div ref={wheelMove}>
@@ -84,20 +180,32 @@ const Table = ({ className, style, columns, data, onSort, pickable = false }: Ta
           <table style={{ width: tableWidth }}>
             <tbody>
               <tr>
-                <td className="measure" style={{ width: '80px' }} />
-                {middleColumns.map((column, inx) => (
+                {selectable && (
                   <td
-                    key={'measure' + inx}
-                    className="measure"
-                    style={{ width: column.width || '200px' }}
+                    className="pui-table-fixed-left pui-table-measure"
+                    style={{ width: selectColumnWidth + 'px' }}
                   />
-                ))}
+                )}
+                {leftColumns
+                  .concat(middleColumns)
+                  .concat(rightColumns)
+                  .map((column, inx) => renderMeasureCell(column, inx))}
               </tr>
               <tr>
-                <td className="fixed">选定</td>
-                {middleColumns.map((column, inx) => (
-                  <td key={'head' + inx}>{column.title}</td>
-                ))}
+                {selectable && (
+                  <td className="pui-table-fixed-left pui-table-selectable" style={{ left: 0 }}>
+                    <CheckBox size="small" />
+                  </td>
+                )}
+                {leftColumns.map((column, inx) => {
+                  return renderTitleCell(column, inx, 'left', {
+                    left: fixColumnLeft[inx + (selectable ? 1 : 0)] + 'px'
+                  });
+                })}
+                {middleColumns.map((column, inx) => renderTitleCell(column, inx, null, {}))}
+                {rightColumns.map((column, inx) =>
+                  renderTitleCell(column, inx, 'right', { right: fixColumnRight[inx] + 'px' })
+                )}
               </tr>
             </tbody>
           </table>
@@ -106,30 +214,37 @@ const Table = ({ className, style, columns, data, onSort, pickable = false }: Ta
           <table style={{ width: tableWidth }}>
             <tbody>
               <tr>
-                <td className="measure" style={{ width: '80px' }} />
-                {middleColumns.map((column, inx) => (
+                {selectable && (
                   <td
-                    key={'measure' + inx}
-                    className="measure"
-                    style={{ width: column.width || '200px' }}
+                    className="pui-table-fixed-left  pui-table-measure"
+                    style={{ width: selectColumnWidth + 'px' }}
                   />
-                ))}
+                )}
+                {leftColumns
+                  .concat(middleColumns)
+                  .concat(rightColumns)
+                  .map((column, inx) => renderMeasureCell(column, inx))}
               </tr>
               {data.map((rowData, inx) => (
                 <tr key={'row' + inx}>
-                  <td className="fixed">
-                    <CheckBox size="small" />
-                  </td>
-                  {middleColumns.map((column, inx) => (
-                    <td
-                      key={'col' + inx}
-                      className={column.multiline ? 'pui-table-cell-multiline' : ''}
-                    >
-                      {column.customCell
-                        ? column.customCell(rowData)
-                        : column.key && rowData[column.key]}
+                  {selectable && (
+                    <td className="pui-table-fixed-left pui-table-selectable" style={{ left: 0 }}>
+                      <CheckBox size="small" />
                     </td>
-                  ))}
+                  )}
+                  {leftColumns.map((column, inx) =>
+                    renderDataCell(column, inx, 'left', rowData, {
+                      left: fixColumnLeft[inx + (selectable ? 1 : 0)] + 'px'
+                    })
+                  )}
+                  {middleColumns.map((column, inx) =>
+                    renderDataCell(column, inx, null, rowData, {})
+                  )}
+                  {rightColumns.map((column, inx) =>
+                    renderDataCell(column, inx, 'right', rowData, {
+                      right: fixColumnRight[inx] + 'px'
+                    })
+                  )}
                 </tr>
               ))}
             </tbody>
