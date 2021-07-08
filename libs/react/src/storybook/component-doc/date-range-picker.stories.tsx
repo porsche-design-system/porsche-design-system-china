01d8796368004e971e3369b6c4947c01a9b40a5d
Calidan <calidan@vip.qq.com>
2021-07-08 09:58:31 +0800

    fix: 优化属性

--- libs/react/src/storybook/component-doc/date-range-picker.stories.tsx
+++ libs/react/src/storybook/component-doc/date-range-picker.stories.tsx
@@ -13,8 +13,9 @@ export const DateRangePickerStoryBook = () => {
       <DateRangePicker
         width="400px"
         label="来访日期"
-        placeholders={['请选择开始日期', '请选择结束日期']}
-        onValuesChange={v => {
+        placeholderStartDate="请选择开始日期"
+        placeholderEndDate="请选择结束日期"
+        onValueChange={v => {
           console.log(v)
         }}
       />
@@ -27,9 +28,12 @@ export const DateRangePickerStoryBook = () => {
       <DateRangePicker
         width="400px"
         label="来访日期"
-        placeholders={['开始日期', '结束日期']}
-        values={dates}
-        onValuesChange={setDates}
+        placeholderStartDate="开始日期"
+        placeholderEndDate="结束日期"
+        nameStartDate="startDate"
+        nameEndDate="startDate"
+        value={dates}
+        onValueChange={setDates}
       />
       <br />
       输出值：{JSON.stringify(dates)}
@@ -42,7 +46,8 @@ export const DateRangePickerStoryBook = () => {
       <DateRangePicker
         width="400px"
         label="限定日期"
-        placeholders={['开始日期', '结束日期']}
+        placeholderStartDate="开始日期"
+        placeholderEndDate="结束日期"
         range="2012-10-12,2012-12-12"
       />
       <br />
