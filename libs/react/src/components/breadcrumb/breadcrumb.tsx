16cd6c12b03700f0e3850a0fd0a4992916500ddb
Calidan <calidan@vip.qq.com>
2021-07-09 16:36:23 +0800

    feat: 面包屑搞定

---
+++ libs/react/src/components/breadcrumb/breadcrumb.tsx
@@ -0,0 +1,78 @@
+import React, { CSSProperties } from 'react'
+import { IconAdd } from '@pui/icons'
+import { componentClassNames } from '../../shared/class-util'
+
+import './breadcrumb.scss'
+
+type PUIIcon = typeof IconAdd
+
+interface BreadcrumbItem {
+  text?: string
+  icon?: PUIIcon
+  iconSize?: number
+  path?: string
+}
+
+export interface BreadcrumbProps {
+  /** 类名 */
+  className?: string
+
+  /** 样式 */
+  style?: CSSProperties
+
+  /* 导航内容 */
+  items?: BreadcrumbItem[]
+
+  /* 点击事件 */
+  onClick?: (item: BreadcrumbItem) => void
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+const Breadcrumb = ({
+  className,
+  style,
+  items = [],
+  onClick
+}: BreadcrumbProps) => {
+  return (
+    <div
+      className={componentClassNames('pui-breadcrumb', {}, className)}
+      style={style}
+    >
+      {items.map((item, inx) => {
+        const Icon = item.icon
+        return (
+          <span key={'breadcrumb' + inx}>
+            <div
+              onClick={() => {
+                if (item.path) {
+                  onClick && onClick(item)
+                }
+              }}
+              className={
+                'pui-breadcrumb-item ' +
+                (item.path ? '' : 'pui-breadcrumb-has-no-path')
+              }
+            >
+              {Icon && (
+                <Icon
+                  style={{
+                    fontSize: item.iconSize ? item.iconSize + 'px' : ''
+                  }}
+                />
+              )}
+              {item.text}
+            </div>
+            {inx !== items.length - 1 && (
+              <div className="pui-breadcrumb-slash"> / </div>
+            )}
+          </span>
+        )
+      })}
+    </div>
+  )
+}
+
+export { Breadcrumb, BreadcrumbItem }
