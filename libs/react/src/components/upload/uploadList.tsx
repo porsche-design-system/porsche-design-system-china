c8baa164204529abbc9bf96ac38ad289d9e15e69
man.wang <man.wang@symbio.com>
2021-07-22 10:12:00 +0800

    feat:upload 上传优化交互

--- libs/react/src/components/upload/uploadList.tsx
+++ libs/react/src/components/upload/uploadList.tsx
@@ -1,4 +1,5 @@
 import React, { FC, } from "react";
+import { CSSTransition } from 'react-transition-group'
 import { Progress } from '../index'
 import { UploadedFile } from "./upload";
 import {
@@ -21,7 +22,6 @@ const UploadList: FC<UploadListProps> = (props) => {
     <ul className='pui-upload-list'>
       {
         fileList.map(file => {
-          console.log(file)
           return (
             <li className={`pui-upload-list-item pui-upload-list-item-${file.status}`} key={file.uid}>
               <div className='pui-upload-list-fileInfo'>
@@ -36,10 +36,11 @@ const UploadList: FC<UploadListProps> = (props) => {
                   <IconDelete onClick={() => onRemove(file)} className='delete-actions' />
                 </span>
               </div>
-              {
-                file.status === 'uploading' &&
+
+              <CSSTransition in={file.status === 'uploading'} ummountOnExit={true} classNames="pui-upload-progress" timeout={1000} >
                 <Progress percent={file.percent || 0} />
-              }
+              </CSSTransition>
+
             </li>
           )
         })
