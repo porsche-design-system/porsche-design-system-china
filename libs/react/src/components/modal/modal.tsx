c9bb4c7ed1488b2e167ad9f6ee3d2dad969a7a40
liu yi <yolaucn@gmail>
2021-07-23 17:41:14 +0800

    feat: modal添加分割线及title icon

--- libs/react/src/components/modal/modal.tsx
+++ libs/react/src/components/modal/modal.tsx
@@ -1,5 +1,5 @@
-import { IconArrowHeadRight, IconClose } from '@pui/icons'
-import React, { CSSProperties, ReactNode, useEffect, useState } from 'react'
+import { IconArrowHeadRight, IconClose, IconClock,IconInformation,IconExclamation } from '@pui/icons'
+import React, { CSSProperties, ReactNode, useEffect, useState ,ReactElement} from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from '..'
 import { componentClassNames } from '../../shared/class-util'
@@ -18,14 +18,33 @@ export interface ModalProps {
   /** 标题 */
   title?: string
 
+  /** 标题左侧Icon */
+  titleIcon?:ReactElement | undefined ,
+
+    /** 标题左侧Iconl类型 */
+  titleIconType?:undefined | 'info' | 'success' | 'warning' |'error'
+
+  /** 副标题 */
+  subtitle?: string
+
   /** 对话框是否可见 */
   visible?: boolean
+
+  /** 头部页脚是否用细线隔开 */
+  hasDivider?: boolean 
 
   /** 确认按钮文字 */
   okText?: string
 
   /** 取消按钮文字 */
   cancelText?: string
+
+   /** 确认按钮Icon */
+  okIcon?:  ReactElement | undefined | null
+
+  /** 取消按钮Icon */
+  cancelIcon?: ReactElement | undefined | null
+  
 
   /* 点击确定回调 */
   onOk?: () => void | Promise<unknown>
@@ -42,6 +61,8 @@ export interface ModalProps {
   /* 显示关闭按钮 */
   showClose?: boolean
 
+  
+
   modalRef?: any
 }
 
@@ -51,19 +72,34 @@ const Modal = ({
   className,
   visible = false,
   title,
+  titleIcon,
+  titleIconType,
+  subtitle = '',
+  hasDivider = false,
   children,
   okText = '确认',
   cancelText = '取消',
+  okIcon,
+  cancelIcon,
   onOk,
   onCancel,
   showCancel = true,
   showOk=true,
   showClose = true,
+
   modalRef
 }: ModalProps) => {
   const [show, setShow] = useState(visible)
   const [isLoading, setIsLoading] = useState(false)
   modalSetIsLoading = setIsLoading
+  const TitleIcon={
+    info:()=><IconInformation/>,
+    success:()=><IconClock/>,
+    warning:()=><IconExclamation/>,
+    error:()=><IconClose/>,
+    undefined:()=>null
+  }
+
 
   useEffect(() => {
     setShow(visible)
@@ -71,12 +107,12 @@ const Modal = ({
   return ReactDOM.createPortal(
     <div
       ref={modalRef}
-      className={componentClassNames('pui-modal-root', { hide: !show + '' })}
+      className={componentClassNames("pui-modal-root", { hide: !show + '' })}
      
     >
       <div className="pui-modal-mask" />
       <div className="pui-modal-wrap">
-        <div className={componentClassNames('pui-modal',{}, className)}  style={style} >
+        <div className={componentClassNames("pui-modal",{}, className)}  style={style} >
           <div className="pui-modal-content">
             {showClose && (
               <div
@@ -89,15 +125,21 @@ const Modal = ({
               </div>
             )}
 
-            <div className="pui-modal-header">
-              <div className="pui-modal-title">{title}</div>
+            <div className={componentClassNames("pui-modal-header",{divider: hasDivider+''})}>
+              <div className="pui-modal-title">
+                {titleIconType && <div className={componentClassNames("pui-modal-title-icon",{type:titleIconType})}>
+                  {titleIcon?titleIcon:TitleIcon[titleIconType]()}
+                  </div>}
+                {title}
+              </div>
+              {subtitle && <div className="pui-model-subtitle">{subtitle}</div>}
             </div>
             <div className="pui-modal-body">{children}</div>
-            <div className="pui-modal-footer">
+            <div className={componentClassNames("pui-modal-footer",{divider: hasDivider+''})}>
               {showCancel && (
                 <Button
                   onClick={() => onCancel && onCancel()}
-                  icon={<IconClose />}
+                  icon={ cancelIcon===null?undefined:cancelIcon===undefined?<IconArrowHeadRight />:cancelIcon}
                   marginRight="10px"
                 >
                   {cancelText}
@@ -107,7 +149,7 @@ const Modal = ({
                 <Button
                 type="primary"
                 loading={isLoading}
-                icon={<IconArrowHeadRight />}
+                icon={okIcon===null?undefined:okIcon===undefined?<IconArrowHeadRight />:okIcon}
                 onClick={() => {
                   if (onOk) {
                     const loadingPromise = onOk()
@@ -243,4 +285,76 @@ Modal.confirm = (
   )
 }
 
+
+Modal.show = (
+ {
+   title,
+   titleIcon,
+   titleIconType,
+   subtitle,
+   showClose,
+   okText,
+   okIcon,
+   onOk,
+   cancelText,
+   cancelIcon,
+   onCancel,
+   children
+ }:ModalProps
+) => {
+  modalCounter++
+  const modalId = '$ModalContainer-' + modalCounter
+  let modalContainer = document.getElementById(modalId)
+  if (!modalContainer) {
+    modalContainer = document.createElement('div')
+    modalContainer.id = modalId
+    document.body.appendChild(modalContainer)
+  }
+
+  let currentPop: any = null
+  ReactDOM.render(
+    <Modal
+      title={title}
+      titleIcon={titleIcon}
+      titleIconType={titleIconType}
+      subtitle={subtitle}
+      okText={okText}
+      okIcon={okIcon}
+      showClose={showClose}
+      onCancel={() => {
+        document.body.removeChild(modalContainer!)
+        document.body.removeChild(currentPop)
+        onCancel && onCancel()
+      }}
+      cancelText={cancelText}
+      cancelIcon={cancelIcon}
+      modalRef={(r: any) => {
+        currentPop = r
+      }}
+      onOk={() => {
+        if (onOk) {
+          const loadingPromise = onOk()
+          if (loadingPromise) {
+            modalSetIsLoading(true)
+            ;(loadingPromise as Promise<unknown>).then(() => {
+              document.body.removeChild(modalContainer!)
+              document.body.removeChild(currentPop)
+              modalSetIsLoading(false)
+            })
+          } else {
+            document.body.removeChild(modalContainer!)
+            document.body.removeChild(currentPop)
+          }
+        } else {
+          document.body.removeChild(modalContainer!)
+          document.body.removeChild(currentPop)
+        }
+      }}
+      visible
+    >
+      {children}
+    </Modal>,
+    modalContainer
+  )
+}
 export { Modal }
