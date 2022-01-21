9a4cd9dfb78aa4a9cf481b60d7e62cbc25c54878
Calidan <calidan@vip.qq.com>
2022-01-21 10:49:04 +0800

    docs: 更新changelog

---
+++ libs/react/CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+### [0.1.0](///compare/v1.2.1...v1.2.2) (2022-01-21)
+
+### Fixed
+
+- 修正 InputNumber 组件小尺寸显示 bug
--- libs/react/src/storybook/coding/change-logs.stories.tsx
+++ libs/react/src/storybook/coding/change-logs.stories.tsx
@@ -2,20 +2,19 @@
 import React from 'react'
 import './doc.scss'
 import './change-logs.scss'
+// @ts-ignore
+import changeLog from '../../../CHANGELOG.md'
+// @ts-ignore
+const MarkdownIt = require('markdown-it')
+
+const md = new MarkdownIt()
 
 const ChangeLogs = () => (
-  <div className="custom-doc">
-    <h1>Change Logs</h1>
-    <div>Ver 0.1.0</div>
-    <div className="release-time">发布时间：2022-01-18 13:30</div>
-    <div>
-      <ul>
-        <li>修正Search组件清除按钮遮挡bug</li>
-        <li>隐藏Search组件字数限制文字</li>
-        <li>修正input-number小尺寸显示错误</li>
-      </ul>
-    </div>
-  </div>
+  <div
+    className="custom-doc"
+    // eslint-disable-next-line react/no-danger
+    dangerouslySetInnerHTML={{ __html: md.render(changeLog) }}
+  />
 )
 
 export default {
