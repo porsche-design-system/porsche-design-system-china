628316b6b364a2d9d75ce299d11f878b4a372aae
Calidan <calidan@vip.qq.com>
2022-06-06 18:07:14 +0800

    fix: 优化包

--- @pui/react/.storybook/main.js
+++ @pui/react/.storybook/main.js
@@ -2,29 +2,19 @@ const path = require('path')
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [{ name: '@storybook/addon-docs', options: { configureJSX: true } }],
+  addons: ['@storybook/preset-scss'],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite'
+  },
+  features: {
+    storyStoreV7: true
+  },
   webpackFinal: config => {
-    config.module.rules.push(
-      {
-        test: /\.(scss|sass)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              indentedSynctax: true,
-              additionalData: `$pui-theme: dark;`
-            }
-          }
-        ],
-        include: path.resolve(__dirname, '../src')
-      },
-      {
-        test: /\.svg$/,
-        use: ['@svgr/webpack', 'url-loader']
-      }
-    )
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'url-loader']
+    })
 
     return config
   }
