c41b30624dcda01ac8c7409e8d9c676789076004
man.wang <man.wang@symbio.com>
2022-07-21 16:22:01 +0800

    feat: 增加面包屑的路由匹配功能以及当前路由放大显示

--- @pui/react/src/components/breadcrumb/breadcrumb.tsx
+++ @pui/react/src/components/breadcrumb/breadcrumb.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties, ReactNode } from 'react'
 import { IconAdd } from '@pui/icons'
 import classNames from 'classnames'
+import { trimEnd } from 'lodash'
 import { componentClassNames } from '../../shared/class-util'
 import { useDefaultSize } from '../../shared/hooks'
 
@@ -9,10 +10,19 @@ import './breadcrumb.scss'
 type PUIIcon = typeof IconAdd
 
 export interface BreadcrumbItem {
-  text?: string
+  text?: ReactNode
   icon?: PUIIcon
   iconSize?: number
   path?: string
+  /** 单独的分隔符 */
+  separator?: ReactNode
+}
+// 为了满足react-router路由类型
+export interface RoutesProps {
+  children?: RoutesProps[];
+  path: string;
+  title: string;
+  [propName: string]: any
 }
 
 export interface BreadcrumbProps {
@@ -24,14 +34,51 @@ export interface BreadcrumbProps {
 
   /** 样式 */
   style?: CSSProperties
-  /* 大小 */
+
+  /** 大小 */
   size?: 'small' | 'medium'
 
-  /* 导航内容 */
+  /** 导航内容 */
   items?: BreadcrumbItem[]
 
-  /* 点击事件 */
+  /** 路由关系，当items为空的时候有效 */
+  routes?: RoutesProps[]
+
+  /** 当前路由，与routes结合使用 */
+  currentPath?: string
+
+  /** 当前路由是否放大显示 */
+  current?: boolean
+
+  /** 点击事件 */
   onClick?: (item: BreadcrumbItem) => void
+}
+
+const realPath = function (path: string): string {
+  return path.startsWith('/') ? path : '/' + path;
+}
+
+// 将嵌套的路由关系转为顺序关系：
+const parseBreadcrumbFromRoutes = (breadcrumbResource: RoutesProps[], pathname: string) => {
+  let result: BreadcrumbItem[] = [], temporary: BreadcrumbItem[] = [];  // 临时数组用于存储每个遍历的结果
+  const rec = (breadcrumbResource: RoutesProps[], temporaryParam: BreadcrumbItem[]) => {
+    breadcrumbResource.forEach(item => {
+      const temporary: BreadcrumbItem[] = [...temporaryParam];   // 遍历中每次都需要根据原来的临时数组创建新的临时数组，不能只用一个
+      const parentPath = temporary[temporary.length - 1]?.path || '';
+      const completePath = parentPath === '/' ? realPath(item.path) : parentPath + realPath(item.path);
+      if (pathname === completePath) {   // 找到与当前页面路由匹配的那个
+        temporary.push({ path: completePath, text: item.title });
+        result = temporary;     // 到了这一步临时文件就是最终的结果
+      } else {
+        if (Array.isArray(item.children) && item.children.length) {
+          temporary.push({ path: completePath, text: item.title });
+          rec(item.children, temporary);
+        }
+      }
+    });
+  }
+  rec(breadcrumbResource, temporary);
+  return result;
 }
 
 const Breadcrumb = ({
@@ -39,10 +86,28 @@ const Breadcrumb = ({
   className,
   style,
   size,
+  routes,
+  currentPath,
   items = [],
+  current,
   onClick
 }: BreadcrumbProps) => {
-  const [defaultSize] = useDefaultSize()
+  const [defaultSize] = useDefaultSize();
+  let breadcrumbItems = items;
+  if (items?.length === 0 && Array.isArray(routes) && currentPath) {
+    // 去掉path后面的斜杠/
+    currentPath = currentPath.length === 1 ? currentPath : (currentPath.endsWith('/') ? trimEnd(currentPath, '/') : currentPath);
+    breadcrumbItems = parseBreadcrumbFromRoutes(routes, currentPath);
+  }
+
+  let currentItem: BreadcrumbItem | null = null;
+  if (current) {
+    if (breadcrumbItems.length > 1 && !currentItem) {
+      // 此处不能用pop，否则会不停剪掉breadcrumbItems
+      currentItem = breadcrumbItems[breadcrumbItems.length - 1] as BreadcrumbItem;
+      breadcrumbItems = breadcrumbItems.slice(0, breadcrumbItems.length - 1);
+    }
+  }
 
   return (
     <div
@@ -53,7 +118,10 @@ const Breadcrumb = ({
       )}
       style={style}
     >
-      {items.map((item, inx) => {
+      {
+        current ? <div className='pui-breadcrumb-currentItem'>{currentItem?.text}</div> : null
+      }
+      {breadcrumbItems.map((item, inx) => {
         const Icon = item.icon
         return (
           <span key={'breadcrumb' + inx}>
@@ -76,8 +144,8 @@ const Breadcrumb = ({
               )}
               {item.text}
             </div>
-            {inx !== items.length - 1 && (
-              <div className="pui-breadcrumb-separator">{separator || '/'}</div>
+            {inx !== breadcrumbItems.length - 1 && (
+              <div className="pui-breadcrumb-separator">{item.separator || separator}</div>
             )}
           </span>
         )
@@ -87,6 +155,8 @@ const Breadcrumb = ({
 }
 
 Breadcrumb.defaultProps = {
-  separator: '/'
+  separator: '/',
+  currentPath: '/',
+  current: false
 }
 export { Breadcrumb }
