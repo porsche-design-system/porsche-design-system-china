2c74957e02493a73b0f8175aa06a632bc25542f5
kevink <kevink@wicresoft.com>
2023-01-30 15:31:50 +0800

    feat: DRSCN-26474|自定义footer

--- @pui/react/src/storybook/component-doc/notification.stories.tsx
+++ @pui/react/src/storybook/component-doc/notification.stories.tsx
@@ -176,3 +176,54 @@ export const NotificationStoryBook4 = () => {
   )
 }
 NotificationStoryBook4.storyName = '位置'
+
+const link = () => {
+  console.log('link to ul')
+}
+
+const doSomething = () => {
+  console.log('doSomething')
+}
+export const NotificationStoryBook5 = () => {
+  const config = {
+    type: 'info',
+    showButton: true,
+    message: '通知标题',
+    description:
+      '通知内容，用于系统主动发起的提示、引导，而非操作反馈。上下内边距固定，自适应高度。',
+    footer: (
+      <div style={{ width: '90%', margin: '0px auto', marginTop: '20px' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+        >
+          <div>
+            <Button onClick={link} type="link">
+              链接按钮
+            </Button>
+          </div>
+          <div>
+            <Button onClick={doSomething} type="default">
+              次要按钮
+            </Button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  const onClick = (config?: any) => {
+    Notification.pop(config)
+  }
+
+  return (
+    <div>
+      <Button onClick={() => onClick({ ...config })} type="default">
+        自定义footer
+      </Button>
+    </div>
+  )
+}
+NotificationStoryBook5.storyName = '自定义footer'
