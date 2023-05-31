c7116545ba27585c3f3b24e47b4f70e82581c17e
yannic <yannic@wicresoft.com>
2023-05-31 09:53:16 +0800

    test(CMREFCN-719): 添加table组件测试

--- @pui/react/setupTests.ts
+++ @pui/react/setupTests.ts
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
+import { TextEncoder, TextDecoder } from 'util'
+
+if (typeof window !== 'undefined') {
+  Object.defineProperty(window, 'TextEncoder', { writable: true, value: TextEncoder });
+  Object.defineProperty(window, 'TextDecoder', { writable: true, value: TextDecoder });
+}
