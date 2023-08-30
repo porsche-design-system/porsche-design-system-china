c25e0fe6b0890e2a21bad0c99df84f64d5ef3ab6
shi.wen <shi.wen@thoughtworks.com>
2023-08-30 16:06:23 +0800

    test(CMREFCN-1171): DateRangePicker test ut

--- @pui/react/src/components/date-range-picker/_tests_/date-range-picker.test.tsx
+++ @pui/react/src/components/date-range-picker/_tests_/date-range-picker.test.tsx
@@ -94,10 +94,10 @@ describe('date-range-picker', () => {
   it('should render with defaultOpen', () => {
     const dateRange = ['2023-03-27', '2023-06-27'];
 
-    render(<DateRangePicker value={dateRange} showClearButton  defaultOpen />);
+    render(<DateRangePicker value={dateRange} showClearButton defaultOpen />);
 
 
-    const element = document.querySelector('.pui-date-range-picker-calendar-head') as HTMLElement
+    const element = document.querySelector('.pui-date-range-picker-calendar-head') as HTMLElement;
 
     expect(element).toBeTruthy();
 
@@ -111,15 +111,271 @@ describe('date-range-picker', () => {
 
     render(<DateRangePicker value={dateRange} showClearButton defaultOpen onValueChange={mockChange} />);
 
-    const blocks = screen.getAllByText('30')
+    const blocks = screen.getAllByText('30');
 
     await userEvent.click(blocks[0]);
 
     expect(mockChange).toBeCalled();
+  });
 
+
+  it('should be unavailable dates out of range', () => {
+    const dateRange = ['2023-03-27', '2023-03-28'];
+    render(<DateRangePicker showClearButton defaultOpen range={dateRange} />);
+
+    const unavailableElement = document.querySelector('.pui-date-range-picker-calendar-unavailable') as HTMLElement;
+
+    expect(unavailableElement).toBeTruthy();
 
   });
 
+  it('should select automatically when set initial date', async () => {
+    const value = ['2023-03-01', '2023-03-02'];
+
+    render(<DateRangePicker value={value} />);
+
+    const dateInit = screen.getByText('2023-03-01');
+
+    await userEvent.click(dateInit);
+
+    const initialDate = screen.getAllByText('1')[0];
+
+    expect(initialDate).toHaveClass('pui-date-range-picker-calendar-picked');
+  });
+
+
+  it('should start date available with range time without selecting initial time', async () => {
+
+    const value = ['2023-03-01', null];
+
+    render(<DateRangePicker range={value} placeholderStartDate='开始日期' placeholderEndDate='结束日期' />);
+
+    const startButton = screen.getByText('开始日期');
+
+    await userEvent.click(startButton);
+
+    const preDate = screen.getAllByText('28')[0];
+    const startDateClassName = screen.getAllByText('1')[0].className;
+
+    expect(preDate).toHaveClass('pui-date-range-picker-calendar-unavailable');
+    expect(startDateClassName).toBe('pui-date-range-picker-calendar-block        ');
+  });
+
+  it('should show calendar today when no set value and range', async () => {
+    render(<DateRangePicker placeholderStartDate='开始日期' placeholderEndDate='结束日期' />);
+
+    const startButton = screen.getByText('开始日期');
+
+    await userEvent.click(startButton);
+
+    const calendarTodayElement = document.querySelector('.pui-date-range-picker-calendar-today') as HTMLElement;
+
+    expect(calendarTodayElement).toBeTruthy();
+  });
+
+  describe('click delete button', () => {
+    it('should delete date initial when click start delete button', async () => {
+      render(<DateRangePicker value={['2023-03-27', '2023-03-28']} showClearButton placeholderStartDate='开始日期' />);
+
+      const deleteButton = document.querySelector('.pui-drpci-start');
+
+      await userEvent.click(deleteButton!);
+
+      const placeholder = screen.getByText('开始日期');
+
+      expect(placeholder).toBeInTheDocument();
+      expect(screen.getByText('2023-03-28')).toBeInTheDocument();
+      expect(screen.queryByText('2023-03-27')).toBeNull();
+    });
+
+    it('should delete date end when click end delete button', async () => {
+      render(<DateRangePicker value={['2023-03-27', '2023-03-28']} showClearButton placeholderEndDate='结束日期' />);
+
+      const deleteButton = document.querySelector('.pui-drpci-end');
+
+      await userEvent.click(deleteButton!);
+
+      const placeholder = screen.getByText('结束日期');
+
+      expect(placeholder).toBeInTheDocument();
+      expect(screen.getByText('2023-03-27')).toBeInTheDocument();
+      expect(screen.queryByText('2023-03-28')).toBeNull();
+    });
+  });
+
+  describe('click Icon Arrow', () => {
+
+    const date = new Date();
+    const actualMouth = date.getMonth() + 1;
+    const actualYear = date.getFullYear();
+
+    it('should go back one year when click IconArrowDoubleLeft', async () => {
+      render(<DateRangePicker defaultOpen />);
+
+      const arrow = screen.getByLabelText('icon_-arrow-double-left');
+
+      const textActual = `${actualYear}年${actualMouth}月`;
+
+      expect(screen.getByText(textActual)).toBeInTheDocument();
+
+      await userEvent.click(arrow);
+
+      const yearPrevious = `${actualYear - 1}年${actualMouth}月`;
+
+      expect(screen.getByText(yearPrevious)).toBeInTheDocument();
+    });
+
+    it('should go forward one year when click IconArrowDoubleRight', async () => {
+      render(<DateRangePicker defaultOpen />);
+
+      const arrow = screen.getByLabelText('icon_-arrow-double-right');
+
+      const textActual = `${actualYear}年${actualMouth}月`;
+
+      expect(screen.getByText(textActual)).toBeInTheDocument();
+
+      await userEvent.click(arrow);
+
+      const yearPrevious = `${actualYear + 1}年${actualMouth}月`;
+
+      expect(screen.getByText(yearPrevious)).toBeInTheDocument();
+    });
+
+    it('should go back one month when click IconArrowHeadLeft', async () => {
+      render(<DateRangePicker defaultOpen />);
+
+      const arrow = screen.getByLabelText('icon_-arrow-head-left');
+
+      const textActual = `${actualYear}年${actualMouth}月`;
+
+      expect(screen.getByText(textActual)).toBeInTheDocument();
+
+      await userEvent.click(arrow);
+
+      const yearPrevious = `${actualYear}年${actualMouth - 1}月`;
+
+      expect(screen.getByText(yearPrevious)).toBeInTheDocument();
+    });
+
+    it('should go forward one month when click IconArrowHeadRight', async () => {
+      render(<DateRangePicker defaultOpen />);
+
+      const arrow = screen.getByLabelText('icon_-arrow-head-right');
+
+      const textActual = `${actualYear}年${actualMouth}月`;
+
+      expect(screen.getByText(textActual)).toBeInTheDocument();
+
+      await userEvent.click(arrow);
+
+      const yearPrevious = `${actualYear}年${actualMouth + 1}月`;
+
+      expect(screen.getByText(yearPrevious)).toBeInTheDocument();
+    });
+  });
+
+  describe('mustPickStartEnd property', () => {
+    it('should clean all picked date when click start delete button', async () => {
+
+      render(<DateRangePicker mustPickStartEnd value={['2023-03-27', '2023-03-28']} showClearButton
+                              placeholderStartDate='开始日期' placeholderEndDate='结束日期' />);
+
+      expect(screen.getByText('2023-03-27')).toBeInTheDocument();
+      expect(screen.getByText('2023-03-28')).toBeInTheDocument();
+
+      const deleteButton = document.querySelector('.pui-drpci-start');
+
+      await userEvent.click(deleteButton!);
+
+      expect(screen.getByText('开始日期')).toBeInTheDocument();
+      expect(screen.getByText('结束日期')).toBeInTheDocument();
+    });
+
+    it('should clean all picked date when click end delete button', async () => {
+
+      render(<DateRangePicker mustPickStartEnd value={['2023-03-27', '2023-03-28']} showClearButton
+                              placeholderStartDate='开始日期' placeholderEndDate='结束日期' />);
+
+      expect(screen.getByText('2023-03-27')).toBeInTheDocument();
+      expect(screen.getByText('2023-03-28')).toBeInTheDocument();
+
+      const deleteButton = document.querySelector('.pui-drpci-end');
+
+      await userEvent.click(deleteButton!);
+
+      expect(screen.getByText('开始日期')).toBeInTheDocument();
+      expect(screen.getByText('结束日期')).toBeInTheDocument();
+    });
+
+  });
+
+
+  describe('range type is string', () => {
+    it('should set range when range property is string', () => {
+      render(<DateRangePicker range='2023-03-01,2023-03-02' defaultOpen/>)
+
+      const unavailableElement = document.querySelector('.pui-date-range-picker-calendar-unavailable') as HTMLElement;
+
+      const outOfRangeDate = screen.getAllByText('3')[0];
+
+      expect(unavailableElement).toBeTruthy();
+      expect(outOfRangeDate).toHaveClass('pui-date-range-picker-calendar-unavailable')
+    });
+
+    it('should set range when range is in xxx days', () => {
+      render(<DateRangePicker range='In3Days' defaultOpen/>);
+
+      const unavailableElement = document.querySelector('.pui-date-range-picker-calendar-unavailable') as HTMLElement;
+
+      expect(unavailableElement).toBeTruthy();
+    })
+  });
+
+  describe('picker box active', () => {
+    it('should active box when select start date', async () => {
+      render(<DateRangePicker placeholderStartDate='开始日期' placeholderEndDate='结束日期'/>)
+
+      const button  = screen.getByText('开始日期');
+
+      await userEvent.click(button);
+
+      const pickerBox = document.querySelector('.pui-date-range-picker-box');
+
+      expect(pickerBox).toHaveClass('pui-date-range-picker-box-active')
+    });
+
+    it('should active end pick box when select start date', async () => {
+      render(<DateRangePicker placeholderStartDate='开始日期' placeholderEndDate='结束日期' />);
+
+      const button  = screen.getByText('开始日期');
+
+      await userEvent.click(button);
+
+      const startDate  = screen.getAllByText('1')[0];
+
+      await userEvent.click(startDate);
+
+      const pickerBox = document.querySelectorAll('.pui-date-range-picker-box')[1];
+
+      expect(pickerBox).toHaveClass('pui-date-range-picker-box-active')
+    });
+
+    it('should active start pick box when select end date', async () => {
+      render(<DateRangePicker placeholderStartDate='开始日期' placeholderEndDate='结束日期' />);
+
+      const button  = screen.getByText('结束日期');
+
+      await userEvent.click(button);
+
+      const startDate  = screen.getAllByText('1')[0];
+
+      await userEvent.click(startDate);
+
+      const pickerBox = document.querySelectorAll('.pui-date-range-picker-box')[0];
+
+      expect(pickerBox).toHaveClass('pui-date-range-picker-box-active');
+    })
+  })
 
 });
 
