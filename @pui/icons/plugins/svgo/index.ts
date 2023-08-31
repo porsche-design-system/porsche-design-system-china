2e6ef3c7a4532d250195046e0a6616103cda1f03
Kaiser Shen <kaisersh@wicrenet.com>
2023-08-31 19:03:12 +0800

    Fix: fix 'viewBox' attribute issue.

--- @pui/icons/plugins/svgo/index.ts
+++ @pui/icons/plugins/svgo/index.ts
@@ -1,11 +1,30 @@
-import { optimize, loadConfig } from "svgo";
+import { optimize, Config } from "svgo";
 import { createTrasformStreamAsync } from "../creator";
 
+const config: Config = {
+  floatPrecision: 2,
+  plugins: [
+    // set of built-in plugins enabled by default
+    {
+      name: "preset-default",
+      params: {
+        overrides: {
+          removeViewBox: false
+        }
+      }
+    },
+    {
+      name: "removeAttrs",
+      params: {
+        attrs: "class"
+      }
+    }
+  ]
+};
 
 export const svgo = () => {
   return createTrasformStreamAsync(async (before) => {
-    await loadConfig();
-    const { data } = optimize(before);
+    const { data } = optimize(before, config as any);
     return data;
   });
 };
