002d75b685330ab88868b5a55ffe9d4772f278ab
Calidan <57343735+calidan-x@users.noreply.github.com>
2023-09-07 15:57:27 +0800

    Update cleanup.sh

--- @pui/icons/scripts/cleanup.sh
+++ @pui/icons/scripts/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ls | grep -v .ptoolsrc.js | grep -v .eslintrc | grep -e '.d.ts' -e '.js$' | xargs rm
+ls | grep -v .ptoolsrc.js | grep -v .eslintrc | grep -e '.d.ts' -e '.js$' | xargs -r rm
 # keep one d.ts to make vscode auto import working with corrcet path
 #rm ./es/icons/*.d.ts || echo
