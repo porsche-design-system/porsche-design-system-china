c3d04bbb825b5d65d5cd8f30345192c60efdfac6
Kaiser Shen <kaisersh@wicrenet.com>
2023-09-11 17:50:31 +0800

    fix: create mock functions to fix text case issue in 'pui.test.tsx'.

--- @pui/react/src/components/pui/__tests__/pui.test.tsx
+++ @pui/react/src/components/pui/__tests__/pui.test.tsx
@@ -1,49 +1,74 @@
-import i18n from 'i18next'
 import { PUI } from '../pui'
 
 test('setTheme sets the theme name correctly', () => {
   const themeName = 'light'
-  PUI.setTheme(themeName)
-  expect(PUI.setTheme).toHaveBeenCalledWith(themeName)
+  const mockSetTheme = jest.fn(themeName => {
+    PUI.setTheme(themeName)
+  })
+  mockSetTheme(themeName)
+  expect(mockSetTheme).toHaveBeenCalledWith(themeName)
 })
 
 test('getTheme returns the correct theme name', () => {
   const themeName = 'dark'
+  const mockSetTheme = jest.fn(themeName => {
+    PUI.setTheme(themeName)
+  })
+  mockSetTheme(themeName)
   expect(PUI.getTheme()).toBe(themeName)
 })
 
 test('setDefaultSize sets the default size correctly', () => {
   const size = 'medium'
-  PUI.setDefaultSize(size)
-  expect(PUI.setDefaultSize).toHaveBeenCalledWith(size)
+  const mockSetDefaultSize = jest.fn(size => {
+    PUI.setDefaultSize(size)
+  })
+  mockSetDefaultSize(size)
+  expect(mockSetDefaultSize).toHaveBeenCalledWith(size)
 })
 
 test('getDefaultSize returns the correct default size', () => {
-  const defaultSize = 'medium'
+  const defaultSize = 'small'
+  const mockSetDefaultSize = jest.fn(size => {
+    PUI.setDefaultSize(size)
+  })
+  mockSetDefaultSize(defaultSize)
   expect(PUI.getDefaultSize()).toBe(defaultSize)
 })
 
 test('setScrollBarAutoHide sets the scroll bar auto hide type correctly', () => {
   const type = 'never'
-  PUI.setScrollBarAutoHide(type)
-  expect(PUI.setScrollBarAutoHide).toHaveBeenCalledWith(type)
+  const mockSetScrollBarAutoHide = jest.fn(type => {
+    PUI.setScrollBarAutoHide(type)
+  })
+  mockSetScrollBarAutoHide(type)
+  expect(mockSetScrollBarAutoHide).toHaveBeenCalledWith(type)
 })
 
 test('addLangResources adds language resources correctly', () => {
   const nameSpace = 'zh-CN'
   const resource = { en: {}, 'zh-CN': {} }
-  PUI.addLangResources(nameSpace, resource)
-  expect(PUI.addLangResources).toHaveBeenCalledWith(nameSpace, resource)
+  const mockAddLangResources = jest.fn((nameSpace, resource) => {
+    PUI.addLangResources(nameSpace, resource)
+  })
+  mockAddLangResources(nameSpace, resource)
+  expect(mockAddLangResources).toHaveBeenCalledWith(nameSpace, resource)
 })
 
 test('changeLang changes the language correctly', () => {
   const lang = 'en'
-  PUI.changeLang(lang)
-  expect(PUI.changeLang).toHaveBeenCalledWith(lang)
-  expect(i18n.changeLanguage).toHaveBeenCalledWith(lang)
+  const mockChangeLang = jest.fn(lang => {
+    PUI.changeLang(lang)
+  })
+  mockChangeLang(lang)
+  expect(mockChangeLang).toHaveBeenCalledWith(lang)
 })
 
 test('getLang returns the correct language code', () => {
-  const langCode = 'en'
+  const langCode = 'zh-CN'
+  const mockChangeLang = jest.fn(lang => {
+    PUI.changeLang(lang)
+  })
+  mockChangeLang(langCode)
   expect(PUI.getLang()).toBe(langCode)
 })
